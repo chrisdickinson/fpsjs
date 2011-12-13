0d501412356e2af992459018de8d197534549930
Chris Dickinson <christopher.s.dickinson@gmail.com>
2011-12-13 15:22:14 -0600

    skeleton should be done

--- media/js/page.js
+++ media/js/page.js
@@ -1,5 +1,13 @@
 var ENTRY_PATH = '/entries/'
 
+function add_class(el, classname) {
+  el.classList.add(classname)
+}
+
+function remove_class(el, classname) {
+  el.classList.remove(classname)
+}
+
 function xhr_get(loc, ready) {
   var xhr = new XMLHttpRequest
   xhr.open('GET', loc)
@@ -18,7 +26,7 @@ function md_get(entry, ready) {
   xhr_get(loc, function(err, text) {
     if(err) return ready(err)
 
-    ready(null, converter.makeHtml(text))
+    ready(null, converter.makeHtml(text.replace(/ -- /g, ' &mdash; ')))
   })
 }
 
@@ -32,6 +40,7 @@ function decorate_entry(entry, ready) {
     , i
 
   if(decorate_entry.cache[entry]) {
+    console.log(decorate_entry.cache[entry])
     return ready(null, decorate_entry.cache[entry])
   }
 
@@ -51,8 +60,10 @@ function decorate_entry(entry, ready) {
       hljs.highlightBlock(el, '  ')
     }
 
-    decorate_entry.cache[entry] = target.children
-    ready(null, target.children)
+    setTimeout(function() {
+      decorate_entry.cache[entry] = [].slice.call(target.children)
+      ready(null, [].slice.call(target.children))
+    }, 100)
   })
 }
 decorate_entry.temporary = document.createElement('div')
@@ -64,15 +75,19 @@ function click_link(ev) {
     , target = click_link.target
     , temporary = click_link.temporary
 
-  console.log(el.href, href)
   if(href.charAt(0) === '#') {
     ev.preventDefault()
     
+    add_class(target, 'loading')
     decorate_entry(href.slice(1), function(err, elements) {
+      remove_class(target, 'loading')
       if(err) return ready(err)
 
+      target.innerHTML = ''
+      console.log(elements)
       for(var i = 0, el; el = elements[i]; ++i) {
-        target.appendChild(el)
+        console.log(el)
+        target.appendChild(elements[i])
       }
 
       push_history(href.slice(1))
@@ -88,9 +103,30 @@ function push_history(loc) {
 }
 
 addEventListener('popstate', function(ev) {
-  console.log(ev)
+  var loc = ev.state
+  if(loc) {
+    add_class(click_link.target, 'loading')
+    decorate_entry(ev.state.replace(/\//g, '_'), function(err, elements) {
+      remove_class(click_link.target, 'loading')
+      if(err) return
+      
+      click_link.target.innerHTML = ''
+      for(var i = 0, el; el = elements[i]; ++i) {
+        click_link.target.appendChild(el)
+      }
+    })  
+  } else {
+    add_class(click_link.target, 'loading')
+    click_link.target.innerHTML = ''
+    for(var i = 0, el; el = base_data[i]; ++i) {
+      click_link.target.appendChild(el)
+    }
+    remove_class(click_link.target, 'loading')
+  }
 })
 
 var links = document.body.getElementsByTagName('a')
+  , base_data = [].slice.call(click_link.target.children)
+
 for(var i = 0, el; el = links[i]; ++i)
   links[i].addEventListener('click', click_link, true)
